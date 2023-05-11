ddad62a38aa49d4cdad9958d858c3a2d15001671
david5garcia <davidgballester@gmail.com>
2023-05-11 20:05:31 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from "./features/Navbar/components/Navbar";
 import Card from "./components/Card";
 import Hero from "./features/Hero/components/Hero";
 import Technologies from "./features/Technologies/components/Technologies";
+import Projects from "./features/Projects/components/Projects";
+import "./components/Card.scss"
 
 function App() {
   return (
@@ -13,16 +15,7 @@ function App() {
       <main>
         <Hero />
         <Technologies />
-        <div className="cards grid gap-6 mx-10 mt-[100vh]">
-          {[...Array(15)].map((_, i) => (
-            <Card
-              key={Date.now()}
-              className="bg-white h-80 my-20 rounded-xl shadow-xl flex justify-center items-center"
-            >
-              <h1 className="text-black text-[36px]">Proyecto {i}</h1>
-            </Card>
-          ))}
-        </div>
+        <Projects />
       </main>
     </>
   );
