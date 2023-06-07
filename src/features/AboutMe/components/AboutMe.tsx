82e08e4ca4e88c50411e574748489af25c3353cb
david5garcia <davidgballester@gmail.com>
2023-06-07 21:28:48 +0200

    Update AboutMe.tsx

--- src/features/AboutMe/components/AboutMe.tsx
+++ src/features/AboutMe/components/AboutMe.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-scroll";
 import MainTitle from "../../../components/MainTitle";
 import useIntersectionObserver from "../../../hooks/useIntersectionObserver";
 
@@ -13,8 +14,11 @@ const AboutMe = () => {
       <MainTitle>About Me</MainTitle>
       <div ref={transitionTop} className="max-w-[40rem]">
         <p className="text-lg">
-          I'm a <span className="text-[#8a2be2] font-semibold">Full Stack Developer</span> at
-          Amazon. Born and raised in Valencia, I have always been passionate
+          I'm a{" "}
+          <span className="text-[#8a2be2] font-semibold">
+            Full Stack Developer
+          </span>{" "}
+          at Amazon. Born and raised in Valencia, I have always been passionate
           about technology and programming. I studied Telecom Engineering, and
           am currently studying a Masters in Web Development.
         </p>
@@ -22,6 +26,20 @@ const AboutMe = () => {
           In my free time I love reading (I'm currently devouring everything
           Brandon Sanderson), and I also love playing sports: baseball,
           handball, tennis and padel being my favourites.
+        </p>
+        <p className="text-lg mt-6">
+          For any job opportunities, projects or if you just want to chat, feel
+          free to{" "}
+          <span className="text-[#8a2be2] font-semibold">
+            {" "}
+            <a
+              href="mailto:davidgballester@gmail.com"
+              className="sm:hover:-translate-y-2 transition-all"
+              target="_blank"
+            >
+              contact me!
+            </a>
+          </span>
         </p>
       </div>
     </div>
