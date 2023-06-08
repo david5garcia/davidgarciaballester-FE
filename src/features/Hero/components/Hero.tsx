09f53f50348b283c10c201d2fd2bbb158ee65072
david5garcia <davidgballester@gmail.com>
2023-06-08 18:57:20 +0200

    center hero

--- src/features/Hero/components/Hero.tsx
+++ src/features/Hero/components/Hero.tsx
@@ -4,50 +4,52 @@ import styles from "../Hero.module.scss";
 const Hero = () => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   return (
-    <div className={`${styles.heroContainer} sm:mt-12 sm:min-h-max`}>
-      <div className="px-6 sm:px-0 animate-fade-right animate-once animate-delay-500 animate-ease-linear">
-        <h1 className={`${styles.heroTitle}`}>Hi there! I'm David&nbsp;👋</h1>
-        <h3 className={`${styles.heroSubTitle}`}>
-          I'm a <span className={`${styles.typed}`}></span>
-        </h3>
-        <div
-          className={`flex flex-row justify-left items-center gap-3 sm:pt-4 pt-2`}
-        >
-          <a
-            className={`${styles.buttonCv} flex flex-row justify-center items-center sm:hover:-translate-y-2 transition-all`}
-            href="/files/CV en ingles.pdf"
-            target="_blank"
-            download
+    <div className={`sm:flex sm:justify-center sm:items-center ${styles.heroContainer}`}>
+      <div className={`${styles.heroContainer} sm:min-h-max`}>
+        <div className="px-6 sm:px-0 animate-fade-right animate-once animate-delay-500 animate-ease-linear">
+          <h1 className={`${styles.heroTitle}`}>Hi there! I'm David&nbsp;👋</h1>
+          <h3 className={`${styles.heroSubTitle}`}>
+            I'm a <span className={`${styles.typed}`}></span>
+          </h3>
+          <div
+            className={`flex flex-row justify-left items-center gap-3 sm:pt-4 pt-2`}
           >
-            <span className="pr-2">
-              <img src="/images/download-white.png" alt="" width={"28px"} />
-            </span>
-            CV
-          </a>
-          <a
-            href="https://www.linkedin.com/in/david-garcia-ballester/"
-            className="sm:hover:-translate-y-2 transition-all"
-            target="_blank"
-          >
-            <img src="/images/linkedin.png" width={`53px`} alt="" />
-          </a>
-          <a
-            href="mailto:davidgballester@gmail.com"
-            className="sm:hover:-translate-y-2 transition-all"
-            target="_blank"
-          >
-            <img src="/images/mail.png" width={`63px`} alt="" />
-          </a>
+            <a
+              className={`${styles.buttonCv} flex flex-row justify-center items-center sm:hover:-translate-y-2 transition-all`}
+              href="/files/CV en ingles.pdf"
+              target="_blank"
+              download
+            >
+              <span className="pr-2">
+                <img src="/images/download-white.png" alt="" width={"28px"} />
+              </span>
+              CV
+            </a>
+            <a
+              href="https://www.linkedin.com/in/david-garcia-ballester/"
+              className="sm:hover:-translate-y-2 transition-all"
+              target="_blank"
+            >
+              <img src="/images/linkedin.png" width={`53px`} alt="" />
+            </a>
+            <a
+              href="mailto:davidgballester@gmail.com"
+              className="sm:hover:-translate-y-2 transition-all"
+              target="_blank"
+            >
+              <img src="/images/mail.png" width={`63px`} alt="" />
+            </a>
+          </div>
         </div>
+        {!isImageLoaded && <div className="h-[50rem]"></div>}
+        <img
+          src="/images/me.png"
+          alt=""
+          placeholder="image of me"
+          className={`${styles.image} animate-fade-left animate-once animate-delay-500 animate-ease-linear`}
+          onLoad={() => setIsImageLoaded(true)}
+        />
       </div>
-      {!isImageLoaded && <div className="h-[50rem]"></div>}
-      <img
-        src="/images/me.png"
-        alt=""
-        placeholder="image of me"
-        className={`${styles.image} animate-fade-left animate-once animate-delay-500 animate-ease-linear`}
-        onLoad={()=>setIsImageLoaded(true)}
-      />
     </div>
   );
 };
