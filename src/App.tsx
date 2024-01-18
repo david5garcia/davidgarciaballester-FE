917794c100ed524f1c24b2708ef620ecfd54c2c4
David Garcia Ballester <davidgballester@uoc.edu>
2024-01-18 23:37:29 +0100

    Google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,20 @@ import Hero from "./features/Hero/components/Hero";
 import Technologies from "./features/Technologies/components/Technologies";
 import Projects from "./features/Projects/components/Projects";
 import AboutMe from "./features/AboutMe/components/AboutMe";
-import "./components/Card.scss"
+import "./components/Card.scss";
 import ArrowLottie from "./features/Hero/components/ArrowLottie";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-9JC19HLV3C");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
   return (
     <>
       <header>
