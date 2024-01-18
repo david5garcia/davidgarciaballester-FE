e62e24786e131143e304506c1d1f1eb298e9cdda
David Garcia Ballester <davidgballester@uoc.edu>
2024-01-19 00:09:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import ReactGA from "react-ga";
-
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
