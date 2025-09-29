51e9398fd72497a9049aa97c589ba69a4a2f3dc6
david5garcia <davidgballester@gmail.com>
2025-09-29 20:49:57 +0200

    eliminar cv

--- src/features/Hero/components/Hero.tsx
+++ src/features/Hero/components/Hero.tsx
@@ -4,7 +4,9 @@ import styles from "../Hero.module.scss";
 const Hero = () => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   return (
-    <div className={`sm:flex sm:justify-center sm:items-center ${styles.heroContainer}`}>
+    <div
+      className={`sm:flex sm:justify-center sm:items-center ${styles.heroContainer}`}
+    >
       <div className={`${styles.heroContainer} sm:min-h-max`}>
         <div className="px-6 sm:px-0 animate-fade-right animate-once animate-delay-500 animate-ease-linear">
           <h1 className={`${styles.heroTitle}`}>Hi there! I'm David&nbsp;👋</h1>
@@ -14,17 +16,6 @@ const Hero = () => {
           <div
             className={`flex flex-row justify-left items-center gap-3 sm:pt-4 pt-2`}
           >
-            <a
-              className={`${styles.buttonCv} flex flex-row justify-center items-center sm:hover:-translate-y-2 transition-all`}
-              href="/files/CV en ingles.pdf"
-              target="_blank"
-              download
-            >
-              <span className="pr-2">
-                <img src="/images/download-white.png" alt="" width={"28px"} />
-              </span>
-              CV
-            </a>
             <a
               href="https://www.linkedin.com/in/david-garcia-ballester/"
               className="sm:hover:-translate-y-2 transition-all"
